c3dc60816f955931eb9ace28553d59eed540655f
felix <felix@volny.co>
2016-03-26 14:15:30 +0800

    moved to react-redux

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { todoApp } from './app'
 import Main from './containers/Main'
-import Provider from './containers/Provider'
+//import Provider from './containers/Provider'
 
 render(
   <Provider store={createStore(todoApp)}>
